65cae44337071ef02fdc6a9b3a53be2d875047bf
Gopesh <medayilgopesh@gmail.com>
2025-12-27 11:33:11 +0100

    (feat) Basic structure for authentication

---
+++ src/main/java/com/terraformgui/backend/TerraformguiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.terraformgui.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraformguiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraformguiBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/terraformgui/backend/auth/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.terraformgui.backend.auth.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public String login(){
+        return "Login successfully";
+    }
+}
